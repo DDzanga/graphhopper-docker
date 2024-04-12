d6fba104460f371a86687d22be7abd1bc679e7cf
Daniel Mwandongo Dzanga <101285144+DDzanga@users.noreply.github.com>
2024-04-12 11:07:45 +0300

    Update gh-update.bat
    Updated to Uganda site

--- gh-update.bat
+++ gh-update.bat
@@ -1,10 +1,10 @@
-: ### This script is suitible for both Linux and Windows ###
+: ### This script is suitable for both Linux and Windows ###
 : # The gh-update.sh is a symbolic link to gh-update.bat
 
 : # Download OSM data if needed
-wget -N http://download.geofabrik.de/central-america/jamaica-latest.osm.pbf
+wget -N https://download.geofabrik.de/africa/uganda-latest.osm.pbf
 : # build new routing data"
-docker-compose run --rm graphhopper import /usr/src/app/jamaica-latest.osm.pbf -o /data/new-gh/
+docker-compose run --rm graphhopper import /usr/src/app/uganda-latest.osm.pbf -o /data/new-gh/
 : # replace the existing routing data
 docker-compose run --rm --entrypoint "bash -c" graphhopper "rm -fr /data/default-gh ; mv /data/new-gh /data/default-gh"
 : # re/start the service as appropriate
